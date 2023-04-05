8381ed50ec8ad6a16b820653b26a9ebfb2a310a0
haseena <haseena03@gmail.com>
2023-04-05 19:05:35 +0530

    yes

--- src/components/Header/Download.jsx
+++ src/components/Header/Download.jsx
@@ -3,20 +3,20 @@ const Download = () => {
     // Function will execute on click of button
     const onButtonClick = () => {
         // using Java Script method to get PDF file
-        fetch('my-cv.pdf').then(response => {
+        fetch('haseena.pdf').then(response => {
             response.blob().then(blob => {
                 // Creating new object of PDF file
                 const fileURL = window.URL.createObjectURL(blob);
                 // Setting various property values
                 let alink = document.createElement('a');
                 alink.href = fileURL;
-                alink.download = 'my-cv.pdf';
+                alink.download = 'haseena.pdf';
                 alink.click();
             })
         })
     }
     return<>
-    {/* <a href="my-cv.pdf" download>click</a> */}
+    {/* <a href="haseena.pdf" download>click</a> */}
         <button className="download-btn" onClick={onButtonClick}>
             Download CV
         </button>
